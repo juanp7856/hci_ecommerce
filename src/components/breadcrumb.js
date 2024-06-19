5dec92be80fcda60bea56a837698f82d2c844aa3
Juan Pablo DC <69323490+juanp7856@users.noreply.github.com>
2024-06-19 15:45:52 -0500

    Update breadcrumb.js

--- src/components/breadcrumb.js
+++ src/components/breadcrumb.js
@@ -12,9 +12,9 @@ const BreadCrumb = ({categorias}) => {
                 </a>
                 </li>
                 {
-                    categorias != [] ? categorias.map((categoria) => {
+                    categorias != [] ? categorias.map((categoria, index) => {
                         return (
-                        <li>
+                        <li key="index">
                             <div className="flex items-center">
                                 <svg className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
