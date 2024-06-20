0e2247a2d9e17531b6e62f718ca61635a87dc7d6
Estebantonio2 <esteban20001118@gmail.com>
2024-06-20 00:59:50 -0500

    añadiendo notitcas + imagenes

---
+++ src/assets/noticias.js
@@ -0,0 +1,19 @@
+const noticias = [
+   {
+      "imagen":"/imagenes/noticia1.jpeg",
+      "titulo":"Próximos locales fisicos",
+      "descripcion":"SpyInv anuncia la apertura de nuevos locales físicos en Nueva York, Londres, Tokio y Sídney. ¡Estas tiendas ofrecerán una experiencia de compra única con demostraciones interactivas y asesoramiento personalizado!"
+   },
+   {
+      "imagen":"/imagenes/noticia2.jpeg",
+      "titulo":"Próximos productos",
+      "descripcion":"SpyInv lanzará próximamente una innovadora línea de productos que incluye la Capa de Invisibilidad Pro 24, el Drone de Espionaje Stealth 360, las Gafas de Visión Oculta V4, y el Bolígrafo Espía Multiusos"
+   },
+   {
+      "imagen":"/imagenes/noticia3.jpeg",
+      "titulo":"Eventos",
+      "descripcion":"SpyInv invita a todos a participar en los eventos exclusivos que tendrán lugar en nuestras nuevas tiendas físicas. Los asistentes podrán ganar premios especiales y aprovechar descuentos únicos"
+   }
+]
+
+export default noticias;
