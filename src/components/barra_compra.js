feb81789d3b5236e25fa1c9a7470358ba3448b00
Juan Pablo DC <juanpab.dc@gmail.com>
2024-06-23 17:42:41 -0500

    Color barra progreso, Router rec pass, ruteo redirect

--- src/components/barra_compra.js
+++ src/components/barra_compra.js
@@ -2,31 +2,31 @@ const Barra = (props) => {
     console.log(props.prog)
     return (
         <>
-            <div id="barra" class="absolute left-0 right-0 mx-auto mt-1.5 bg-white border-black border-2 rounded-lg h-6 max-w-2xl w-full" aria-hidden="true"></div>
-            <div class="max-w-xl mx-auto relative z-10">
-                <div class="flex items-center justify-between">
-                <div class="relative flex items-center">
-                    <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                    <div class="w-full border-t border-black"></div>
+            <div id="barra" className="absolute left-0 right-0 mx-auto mt-1.5 bg-white border-black border-2 rounded-lg h-6 max-w-2xl w-full" aria-hidden="true"></div>
+            <div className="max-w-xl mx-auto relative z-10">
+                <div className="flex items-center justify-between">
+                <div className="relative flex items-center">
+                    <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                    <div className="w-full border-t border-black"></div>
                     </div>
-                    <div class={`relative flex items-center justify-center w-10 h-10 ${props.prog=='/cart' ? 'bg-gray-300' : 'bg-white'} border-black border-2 rounded-full`}>
-                    <span class={props.prog=='/cart' ? 'text-white' : 'text-black'}>1</span>
-                    </div>
-                </div>
-                <div class="relative flex items-center">
-                    <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                    <div class="w-full border-t border-black"></div>
-                    </div>
-                    <div class={`relative flex items-center justify-center w-10 h-10 ${props.prog=='/payment' ? 'bg-gray-300' : 'bg-white'} border-black border-2 rounded-full`}>
-                    <span class={props.prog=='/payment' ? 'text-white' : 'text-black'}>2</span>
+                    <div className={`relative flex items-center justify-center w-10 h-10 ${props.prog=='/cart' ? 'bg-gray-300' : 'bg-white'} border-black border-2 rounded-full`}>
+                    <span className="text-black">1</span>
                     </div>
                 </div>
-                <div class="relative flex items-center">
-                    <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                    <div class="w-full border-t border-black"></div>
+                <div className="relative flex items-center">
+                    <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                    <div className="w-full border-t border-black"></div>
                     </div>
-                    <div class={`relative flex items-center justify-center w-10 h-10 ${props.prog=='/success' ? 'bg-gray-300' : 'bg-white'} border-black border-2 rounded-full`}>
-                    <span class={props.prog=='/success' ? 'text-white' : 'text-black'}>3</span>
+                    <div className={`relative flex items-center justify-center w-10 h-10 ${props.prog=='/payment' ? 'bg-gray-300' : 'bg-white'} border-black border-2 rounded-full`}>
+                    <span className="text-black">2</span>
+                    </div>
+                </div>
+                <div className="relative flex items-center">
+                    <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                    <div className="w-full border-t border-black"></div>
+                    </div>
+                    <div className={`relative flex items-center justify-center w-10 h-10 ${props.prog=='/success' ? 'bg-gray-300' : 'bg-white'} border-black border-2 rounded-full`}>
+                    <span className="text-black">3</span>
                     </div>
                 </div>
                 </div>
--- src/pages/register.js
+++ src/pages/register.js
@@ -38,11 +38,11 @@ const Register = () => {
                             <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Acepto las <Link href="/policies" className="justify-end text-white">Políticas</Link></label>
                         </div>
                         <div className="flex justify-center">
-                            <button onClick={()=>router.push('/login')} type="submit" class="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">CREAR NUEVA CUENTA</button>
+                            <button onClick={()=>router.push('/')} type="submit" class="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">CREAR NUEVA CUENTA</button>
                         </div>
                         <div className="flex justify-center">
                             <p className='mx-1'>¿Tienes una cuenta?</p>
-                            <Link href="/rec-pass" className="justify-end text-white font-bold">Ingresa aquí</Link>
+                            <Link href="/login" className="justify-end text-white font-bold">Ingresa aquí</Link>
                         </div>
                     </form>
                 </div>
